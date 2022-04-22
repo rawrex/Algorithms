868d1ce346c1a0a6e70755c55b46db52ff62c32a
rawrex <rawrex@protonmail.ch>
2022-04-22 18:42:34 +0700

    Initial commit.

---
+++ binary_search.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+const std::vector<const int> data = {-18, -3, 0, 1, 2, 5, 11, 23, 53, 199, 1329};
+
+// We can return:
+// 1. Index
+// 2. An iterator to the element
+// 3. Boolean as whether the element is present
+// We can get the index and the fact of presence of the element using the iterator
+
+template <typename T>
+
+
+int main() {
+
+}
