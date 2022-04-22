2ca68205b1f8965b2804af384fdf4a46ec9bced3
rawrex <rawrex@protonmail.ch>
2022-04-22 18:51:10 +0700

    Added the decalration for initial template func.

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-const std::vector<const int> data = {-18, -3, 0, 1, 2, 5, 11, 23, 53, 199, 1329};
+const std::vector<int> data = {-18, -3, 0, 1, 2, 5, 11, 23, 53, 199, 1329};
 
 // We can return:
 // 1. Index
@@ -9,7 +9,9 @@ const std::vector<const int> data = {-18, -3, 0, 1, 2, 5, 11, 23, 53, 199, 1329}
 // 3. Boolean as whether the element is present
 // We can get the index and the fact of presence of the element using the iterator
 
-template <typename T>
+// C++11
+template <typename Container, typename Element>
+typename Container::const_iterator binsearch(const Container& cont, const Element & elem);
 
 
 int main() {
