a7e1455ceb6bf0dac6b3b5cd569b0b2bcb651d71
rawrex <rawrex@protonmail.ch>
2022-05-27 23:04:17 +0700

    Added more comments and cleaned up in the Insertion sort Linked list.

--- insertion_sort_linked_list.cpp
+++ insertion_sort_linked_list.cpp
@@ -21,6 +21,7 @@ void print(ListNode* head)
 	std::cout << std::endl;
 }
 
+// Define the test list
 ListNode node5;
 ListNode node4(-4, &node5);
 ListNode node3(0, &node4);
@@ -28,7 +29,8 @@ ListNode node2(4, &node3);
 ListNode node1(8, &node2);
 ListNode head(10, &node1);
 
-class Solution{
+class Solution
+{
 	ListNode* insert(ListNode* sorted_head, ListNode* new_node)
 	{
 		// If the sorted_head is a head of an empty list
@@ -43,16 +45,16 @@ class Solution{
 		{
 			ListNode* iter = sorted_head;
 
-			// While the next node in the sorted list is present
+			// While the next node in the sorted list is present (not a nullptr)
 			// And has a value that is less than the value of the new node we add
 			// Advance the iter in the sorted list
 			for (; iter->next && (iter->next->val < new_node->val); iter=iter->next);
 			
 			// The iter now either points to the last element in the sorted list
-			// Or its next node has value greater than the value of the new_node
+			// Or its (iter) next node has value greater than the value of the new_node
 
 			// Iter now points to the new_node
-			// And new_node points to the node that before was coming after the iter
+			// And new_node points to the node that was coming after the iter
 			new_node->next = iter->next;
 			iter->next = new_node;
 		}
@@ -67,7 +69,7 @@ public:
 		ListNode* iter = head;
 		while (iter)
 		{
-			// We need to store the next node since it may be overwritten
+			// We need to store the next node since it may be overwritten (line 41)
 			auto next = iter->next;
 
 			// Insert the iter appropriately
@@ -83,12 +85,8 @@ public:
 
 int main()
 {
-
 	print(&head);
-
 	Solution s;
 	ListNode* result = s.insertionSortList(&head);
-	
 	print(result);
-
 }
