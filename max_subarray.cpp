03420e979e3947f2571354c4bec696d30e6a696f
rawrex <rawrex@proton.me>
2022-07-12 17:03:30 +0700

    Helper functions added. These will need to be refactored later on, when the overal program is finished.

--- max_subarray.cpp
+++ max_subarray.cpp
@@ -29,6 +29,46 @@ result_t FindMaxCrossingSubarray(container_t& container, iter_t& low, iter_t& mi
 	
 }
 
+const std::pair<iter_t, int> FindLeftCrossingSum(iter_t& low, iter_t& mid)
+{
+	// The iterator to the furtherst to the left satisfactory element
+	auto max_left_i = mid;
+	// A sentinel 
+	auto left_result_sum = INT_MIN;
+	auto current_sum = 0;
+
+	for(auto& i = mid; i != low; --i)
+	{
+		current_sum += *i;
+		if (current_sum > left_result_sum)
+		{
+			left_result_sum = current_sum;
+			max_left_i = i;
+		}
+	}
+	return std::pair<iter_t, int>(max_left_i, current_sum);
+}
+
+// We should refactor these two later
+const std::pair<iter_t, int> FindRightCrossingSum(iter_t& mid, iter_t& high)
+{
+	// The iterator to the furtherst to the left satisfactory element
+	auto max_right_i = mid+1;
+	// A sentinel 
+	auto right_result_sum = INT_MIN;
+	auto current_sum = 0;
+
+	for(auto& i = mid; i != high; --i)
+	{
+		current_sum += *i;
+		if (current_sum > right_result_sum)
+		{
+			right_result_sum = current_sum;
+			max_right_i = i;
+		}
+	}
+	return std::pair<iter_t, int>(max_right_i, current_sum);
+}
 
 
 int main() {
