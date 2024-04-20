4d5da354d8a3e1caa357626e9c6c7161c3695d7a
rawrex <rawrex@proton.me>
2024-04-20 16:58:45 +0700

    Added first attempt. Some of the test cases are not passed.

---
+++ maximum_segment_sum_after_removals/src/main.cpp
@@ -0,0 +1,97 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> n1 { 1, 2, 5, 6, 1 };
+vector<int> q1 { 0, 3, 2, 4, 1 };
+
+// [16,5,3,0]
+vector<int> n2 {3,2,11,1};
+vector<int> q2 {3,2,1,0};
+
+class Solution 
+{
+public:
+    vector<long long> solve(vector<int>& nums, vector<int>& removeQueries) 
+    {
+        m_size = nums.size();
+        return naive(nums, removeQueries);
+    }
+
+private:
+    using result_container_t = vector<long long>;
+
+    result_container_t naive(vector<int>& nums, vector<int>& queries) noexcept
+    {
+        result_container_t result(m_size);
+
+	int counter = 0;
+        for(auto index : queries)
+        {
+            nums[index] = 0;
+            result[counter++] = std::max(maxSumLeft(nums, index), maxSumRight(nums, index));
+        }
+        return result;
+    }
+
+    long long maxSumLeft(const vector<int>& nums, int index) const noexcept
+    {
+        long long current_sum = 0;
+	long long max_sum = 0;
+
+        for(/* empty */; index != 0; --index)
+        {
+		if(nums[index] != 0)
+		{
+			current_sum += nums[index];
+		}
+		else
+		{
+			max_sum = std::max(max_sum, current_sum);
+			current_sum = 0;
+		}
+	}
+
+	max_sum = std::max(max_sum, current_sum);
+        return max_sum;
+    }
+
+    long long maxSumRight(const vector<int>& nums, int index) const noexcept
+    {
+        long long current_sum = 0;
+        long long max_sum = 0;
+
+        for(/* empty */; index != m_size; ++index)
+        {
+		if(nums[index] != 0)
+		{
+			current_sum += nums[index];
+		}
+		else
+		{
+			max_sum = std::max(max_sum, current_sum);
+			current_sum = 0;
+		}
+        }
+
+	max_sum = std::max(max_sum, current_sum);
+        return max_sum;
+    }
+
+private:
+    int            m_size;
+};
+
+void print(const vector<long long>& vec)
+{
+	for(const auto& i : vec)
+		std::cout << i << ' ';
+	std::cout << std::endl;
+}
+
+int main()
+{
+	auto result = Solution().solve(n2, q2);
+	print(result);
+}
