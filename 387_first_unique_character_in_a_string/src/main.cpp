622a014b6429d99d9107f6adc0cf12a15f58735f
rawrex <rawrex@proton.me>
2024-06-07 16:04:15 +0700

    Added a solution that does not mitigates a case where there are not unique value.

--- 387_first_unique_character_in_a_string/src/main.cpp
+++ 387_first_unique_character_in_a_string/src/main.cpp
@@ -4,6 +4,24 @@
 
 struct Solution
 {
+	int firstUniqChar(const std::string& input) noexcept
+	{
+		// Go in reverse and remember the last unique character
+		int last_unique = -1;
+
+		for (int index = input.size(); index >= 0; --index)
+		{
+			char current_ch = input[index];
+			// If we have not seen such character, remember it and update the current unique char
+			if (m_tracker.emplace(current_ch).second)
+				last_unique = index;
+		}
+
+		return last_unique;
+	}
+
+private:
+	std::unordered_set<char> m_tracker;
 
 };
 
