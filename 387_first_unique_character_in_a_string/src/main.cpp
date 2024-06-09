fda8661ef42c0cdacaf0ffda9d69fb6fae37ecd5
rawrex <rawrex@proton.me>
2024-06-09 18:12:00 +0700

    Added a lamest solution ever, thankfully!

--- 387_first_unique_character_in_a_string/src/main.cpp
+++ 387_first_unique_character_in_a_string/src/main.cpp
@@ -2,30 +2,39 @@
 #include <string>
 #include <unordered_set>
 
+const std::string foo = "loveleetcode";
+const std::string bar = "aabb";
+
 struct Solution
 {
 	int firstUniqChar(const std::string& input) noexcept
 	{
-		// Go in reverse and remember the last unique character
-		int last_unique = -1;
-
-		for (int index = input.size(); index >= 0; --index)
+		for (int i = 0; i != input.size(); ++i)
 		{
-			char current_ch = input[index];
-			// If we have not seen such character, remember it and update the current unique char
-			if (m_tracker.emplace(current_ch).second)
-				last_unique = index;
+			if (!m_tracker.emplace(input[i]).second)
+				continue;
+
+			int j = i+1;
+
+			for (; j != input.size(); ++j)
+			{
+				if (input[i] == input[j])
+					break;
+			}
+
+			if (j == input.size())
+				return i;
 		}
 
-		return last_unique;
+		return -1;
 	}
 
 private:
 	std::unordered_set<char> m_tracker;
-
 };
 
 int main()
 {
-
+	std::cout << Solution().firstUniqChar(foo) << std::endl;
+	std::cout << Solution().firstUniqChar(bar) << std::endl;
 }
