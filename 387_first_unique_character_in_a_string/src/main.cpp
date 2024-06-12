21667a256f06502f9024633b68626612b2e30fd3
rawrex <rawrex@proton.me>
2024-06-12 16:36:49 +0700

    Added a working but slow solution.

--- 387_first_unique_character_in_a_string/src/main.cpp
+++ 387_first_unique_character_in_a_string/src/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
+#include <unordered_map>
 
 struct Solution
 {
 	int firstUniqChar(const std::string& input) noexcept
 	{
+        // return naive(input);
+        return counter(input);
+	}
+
+private:
+    int naive(const std::string& input) noexcept
+    {
 		// Go in reverse and remember the last unique character
 		int last_unique = -1;
 
@@ -18,11 +26,23 @@ struct Solution
 		}
 
 		return last_unique;
+    }
+
+	int counter(const std::string& input) noexcept
+	{
+		for (const auto& ch : input)
+			++m_frequncies[ch];
+
+		for (int i = 0; i != input.size(); ++i)
+			if (m_frequncies[input[i]] == 1)
+				return i;
+
+		return -1;
 	}
 
 private:
 	std::unordered_set<char> m_tracker;
-
+	std::unordered_map<char, std::string::size_type> m_frequncies;
 };
 
 int main()
