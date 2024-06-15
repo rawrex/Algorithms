a2441f7e77c18875be05b193e4a8f4b603c26119
rawrex <rawrex@proton.me>
2024-06-15 13:29:39 +0700

    Added processing of the mul/div expressions and got semi working solution.

--- 227_basic_calculator/src/main.cpp
+++ 227_basic_calculator/src/main.cpp
@@ -7,7 +7,8 @@
 
 using namespace std;
 
-const string test_str = "3*4 - 4 / 2";
+const string test_str_1 = "3*4 - 4 / 2";
+const string test_str_2 = "3+2*2";
 
 class Operator
 {
@@ -24,37 +25,14 @@ public:
     int parse(const string& input) const noexcept
     {
         int result = 0;
-        vector<string> muls_divs;
-        vector<string> sum_dif_ops;
-
-        // Simplify the management of the substrings by cutting the head of the string
-        // This we will need to avoid later
-        string tmp = input;
-
-        auto index = tmp.find_first_of(m_sum_dif);
-        while (index != std::string::npos)
-        {
-            // Collect the numbers and mul/div subexpressions first
-            muls_divs.emplace_back(tmp.substr(0, index));
-            // Collect the found sum/dif operators
-            sum_dif_ops.emplace_back(tmp.substr(index, 1));
-
-            // Truncate the just found sum/dif operator from the string
-            tmp = tmp.substr(++index);
-
-            // Try to find the next occurance of the sum/dif operator
-            index = tmp.find_first_of(m_sum_dif);
-        }
-
-        // There is a number or a one or more mul/div subexpressions
-        if (!tmp.empty())
-            muls_divs.emplace_back(tmp);
+        vector<string> sumdif_delimiters;
+        vector<string> muls_divs = split(input, m_sum_dif, sumdif_delimiters);
 
         auto numbers = processMulsDivs(muls_divs);
         
-        for (size_t i = 0; i != sum_dif_ops.size(); ++i)
+        for (size_t i = 0; i != sumdif_delimiters.size(); ++i)
         {
-            const auto& current_operation_str = sum_dif_ops[i];
+            const auto& current_operation_str = sumdif_delimiters[i];
             const auto& lhs = numbers[i];
             const auto& rhs = numbers[i+1];
     
@@ -66,9 +44,58 @@ public:
         return result;
     }
 
+    vector<string> split(const string& input, const string& delimiters, vector<string>& found_delimiters) const
+    {
+        vector<string> result;
+
+        // Simplify the management of the substrings by cutting the head of the string
+        // This we will need to avoid later
+        auto tmp = input;
+        
+        auto index = tmp.find_first_of(delimiters);
+        while (index != std::string::npos)
+        {
+            // Collect the numbers and mul/div subexpressions first
+            result.emplace_back(tmp.substr(0, index));
+
+            // Collect the found sum/dif operators
+            found_delimiters.emplace_back(tmp.substr(index, 1));
+
+            // Truncate the just found sum/dif operator from the string
+            tmp = tmp.substr(++index);
+
+            // Try to find the next occurance of the sum/dif operator
+            index = tmp.find_first_of(m_sum_dif);
+        }
+
+        // There is a number or a one or more mul/div subexpressions
+        if (!tmp.empty())
+            result.emplace_back(tmp);
+
+        return result;
+    }
+
     vector<int> processMulsDivs(const vector<string>& muls_divs) const
     {
         vector<int> result;
+
+        for (const auto& expression : muls_divs)
+        {
+            vector<string> muldiv_delimiters;
+            vector<string> numbers_str = split(expression, m_mul_div, muldiv_delimiters);
+
+            for (size_t i = 0; i != muldiv_delimiters.size(); ++i)
+            {
+				const auto& current_operation_str = muldiv_delimiters[i];
+
+                auto lhs = std::stoi(numbers_str[i]);
+                auto rhs = std::stoi(numbers_str[i + 1]);
+		
+				auto operation = makeOperation(current_operation_str);
+
+                result.emplace_back(operation(lhs, rhs));
+            }
+        }
          
         return result;
     }
@@ -94,7 +121,7 @@ private:
     std::string m_mul_div = "*/";
     std::string m_sum_dif = "+-";
 
-    std::array<std::function<int(int, int)>, 4> m_operators;
+    std::array<std::function<int(int, int)>, 4> m_operators = { std::plus<int>(), std::minus<int>(), std::multiplies<int>(), std::divides<int>() };
 };
 
 class Solution
@@ -115,5 +142,6 @@ private:
 
 int main()
 {
-    std::cout << Solution().calculate(test_str) << std::endl;
+    std::cout << Solution().calculate(test_str_1) << std::endl;
+    // std::cout << Solution().calculate(test_str_2) << std::endl;
 }
