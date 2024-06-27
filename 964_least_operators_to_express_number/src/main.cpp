f935c41606bd56c0a50d8437c037cf666356945d
rawrex <rawrex@proton.me>
2024-06-27 11:41:04 +0700

    Added an outline for a solution attempt.

--- 964_least_operators_to_express_number/src/main.cpp
+++ 964_least_operators_to_express_number/src/main.cpp
@@ -1,15 +1,63 @@
 #include <iostream>
 
+struct Operation
+{
+    enum class Type { mul, div, plus, minus };
+
+    Operation(const Type& type) : m_type(type) {}
+
+    int apply(int lhs, int rhs)
+    {
+        switch(m_type)
+        {
+            case Type::mul:
+                return lhs * rhs;
+            case Type::div:
+                return lhs / rhs;
+            case Type::plus:
+                return lhs + rhs;
+            case Type::minus:
+                return lhs - rhs;
+        }
+    }
+
+    Type m_type;
+};
+
 struct Solution
 {
     int leastOpsExpessTarget(int x, int target)
     {
+        m_x = x;
+        m_target = target;
+
         return firstAttempt(x, target);
     }
+
 private:
     int firstAttempt(int x, int target)
     {
-        return estimate(x, target);
+        auto estimation = estimate(x, target);
+
+        int current = x;
+
+        if(current < target)
+        {
+            search(current, Operation::Type::mul);
+            search(current, Operation::Type::plus);
+        }
+        if(current > target)
+        {
+            search(current, Operation::Type::minus);
+            search(current, Operation::Type::div);
+        }
+
+        return 0;
+    }
+
+    int search(int current_value, Operation op)
+    {
+        return 0; 
     }
 
     int estimate(int x, int target) 
@@ -21,6 +69,9 @@ private:
 
         return e;
     }
+private:
+    int m_x = 0;
+    int m_target = 0;
 };
 
 int main()
