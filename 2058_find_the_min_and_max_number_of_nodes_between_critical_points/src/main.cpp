39ed049c983f72fa9f955ea1bd465fb8ea357616
rawrex <rawrex@proton.me>
2024-07-06 15:42:07 +0700

    Added more logic to the approach as well as a test case.

--- 2058_find_the_min_and_max_number_of_nodes_between_critical_points/src/main.cpp
+++ 2058_find_the_min_and_max_number_of_nodes_between_critical_points/src/main.cpp
@@ -23,32 +23,59 @@ struct Solution
 
 	vector<int> traverse(ListNode* node)
 	{
-		int min_distance = -1;
-		int max_distance = -1;
-
-		int prev_min_value = std::numeric_limits<int>::max();
-		int prev_max_value = std::numeric_limits<int>::min();
-
-		// No!
-		int prev = 0;
-
-		while (node)
+		while (node->next)
 		{
-			// current node is local minimum candidate
-			if (node->val < prev_min_value)
-				; 
+			// current node is local minimum
+			if (node->val < m_current_min && node->val < node->next->val)
+			{
+				m_current_min = node->val;
 
-			// current node is local maximum candidate
-			if (node->val > prev_max_value)
-				; 
+				if (m_current_distance_min > m_record_distance_min)
+					m_record_distance_min = m_current_distance_min;
+			}
+
+			// current node is local maximum
+			if (node->val > m_current_max && node->val > node->next->val)
+			{
+				m_current_max = node->val;
+
+				if (m_current_distance_max > m_record_distance_max)
+					m_record_distance_max = m_current_distance_max;
+			}
+
+			++m_current_distance_min;
+			++m_current_distance_max;
 
 			// iterate further
 			node = node->next;
 		}
+
+		return { m_record_distance_min, m_record_distance_max };
 	}
+
+private:
+	int m_record_distance_min = -1;
+	int m_record_distance_max = -1;
+
+	int m_current_distance_min = -1;
+	int m_current_distance_max = -1;
+
+	int m_current_min = std::numeric_limits<int>::max();
+	int m_current_max = std::numeric_limits<int>::min();
 };
+
+// [5,3,1,2,5,1,2]
+
+ListNode a(2, nullptr);
+ListNode b(1, &a);
+ListNode c(5, &b);
+ListNode d(2, &c);
+ListNode e(1, &d);
+ListNode f(3, &e);
+ListNode test_head(5, &f);
 
 int main()
 {
-
+	auto result = Solution().nodesBetweenCriticalPoints(&test_head);
+	std::cout << result[0] << ' ' << result[1] << std::endl;
 }
