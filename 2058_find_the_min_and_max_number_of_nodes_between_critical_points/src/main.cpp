b8e89a47a1a716653a1ed05f6ba9c1a6b7c53e72
rawrex <rawrex@proton.me>
2024-07-12 17:56:00 +0700

    Corrected the implementation and added more tests. Done the solution.

--- 2058_find_the_min_and_max_number_of_nodes_between_critical_points/src/main.cpp
+++ 2058_find_the_min_and_max_number_of_nodes_between_critical_points/src/main.cpp
@@ -18,31 +18,33 @@ struct Solution
 {
 	vector<int> nodesBetweenCriticalPoints(ListNode* head)
 	{
-		if (!head)
+		if (!head || !head->next || !head->next->next)
 			return { -1, -1 };
 
 		m_prev_value = head->val;
 
-		if (!head->next)
-			return { 1, 1 };
-			
 		return traverse(head->next);
 	}
 
 	vector<int> traverse(ListNode* node)
 	{
-		for (/* empty */; node; ++m_current_distance, m_prev_value = node->val, node = node->next)
+		for (int current_index = 0; node != nullptr; ++current_index, m_prev_value = node->val, node = node->next)
 		{
-			if (!isCritical(node))
-				continue;
-
-			m_record_distance_max = std::max(m_current_distance, m_record_distance_max);
-			m_record_distance_min = std::min(m_current_distance, m_record_distance_min);
-
-			m_current_distance = 0;
+			if (isCritical(node))
+			{
+				if (m_first_index == -1)
+					m_first_index = current_index;
+				else	
+					m_min_distance = std::min(current_index - m_previous_index, m_min_distance);
+				m_previous_index = current_index;
+			}
 		}
 
-		return { m_record_distance_min, m_record_distance_max };
+		// None or only one critical point
+		if (m_first_index == m_previous_index)
+			return { -1, -1 };
+
+		return { m_min_distance, m_previous_index - m_first_index };
 	}
 
 	bool isCritical(const ListNode* node) const noexcept
@@ -55,9 +57,10 @@ struct Solution
 
 private:
 	int m_prev_value = 0;
-	int m_current_distance = 0;
-	int m_record_distance_min = std::numeric_limits<int>::max();
-	int m_record_distance_max = std::numeric_limits<int>::min();
+
+	int m_first_index = -1;
+	int m_previous_index = -1;
+	int m_min_distance = std::numeric_limits<int>::max();
 };
 
 // [5,3,1,2,5,1,2]
@@ -70,8 +73,29 @@ ListNode e(1, &d);
 ListNode f(3, &e);
 ListNode test_head(5, &f);
 
+ListNode a_2(1, nullptr);
+ListNode test_head_2(3, &a_2);
+
+ListNode a_3(2, nullptr);
+ListNode b_3(3, &a_3);
+ListNode c_3(3, &b_3);
+ListNode test_head_3(2, &c_3);
+
+// [1,3,2,2,3,2,2,2,7]
+ListNode h_4(7, nullptr);
+ListNode g_4(2, &h_4);
+ListNode f_4(2, &g_4);
+ListNode e_4(2, &f_4);
+ListNode d_4(3, &e_4);
+ListNode c_4(2, &d_4);
+ListNode b_4(2, &c_4);
+ListNode a_4(3, &b_4);
+ListNode test_head_4(1, &a_4);
 int main()
 {
-	auto result = Solution().nodesBetweenCriticalPoints(&test_head);
+//	auto result = Solution().nodesBetweenCriticalPoints(&test_head);
+//	auto result = Solution().nodesBetweenCriticalPoints(&test_head_2);
+	auto result = Solution().nodesBetweenCriticalPoints(&test_head_3);
+//	auto result = Solution().nodesBetweenCriticalPoints(&test_head_4);
 	std::cout << result[0] << ' ' << result[1] << std::endl;
 }
