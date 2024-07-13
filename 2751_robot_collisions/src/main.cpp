72ab2177f68e3332fa24a0b6d6bd12cb74e6707e
rawrex <rawrex@proton.me>
2024-07-13 14:21:37 +0700

    Added an outline for a basic approach for the solution.

--- 2751_robot_collisions/src/main.cpp
+++ 2751_robot_collisions/src/main.cpp
@@ -3,8 +3,97 @@
 
 using namespace std;
 
-struct Solution 
+class Solution 
 {
+    struct Robot
+    {
+        Robot(int pos, int hp, const string& dir) : m_position(pos), m_hp(hp), m_increment(dir == "R" ? 1 : -1) 
+        {
+
+        }
+
+        int onTick()
+        {
+            return m_position += m_increment; 
+        }
+
+        int position() const noexcept
+        {
+            return m_position;
+        }
+
+        int hp() const noexcept
+        {
+            return m_hp;
+        }
+
+        void kill() noexcept
+        {
+            m_hp = 0;
+        }
+
+        void damage() noexcept
+        {
+            --m_hp;
+        }
+
+        bool isDead() const noexcept
+        {
+            return m_hp == 0;
+        }
+
+    private:
+        int m_position = 0;
+        int m_hp = 0;
+        int m_increment = 0;
+    };
+
+    struct Scene
+    {
+        void tick() 
+        {
+            unordered_map<int, Robot*> position2robot;
+
+            for(auto& current_robot : m_robots)
+            {
+                if(current_robot.isDead())
+                    continue;
+
+                if(!position2robot.emplace(current_robot.onTick(), &current_robot).second)
+                {
+                    auto& other_robot_ptr = position2robot[current_robot.position()];
+                    collide(current_robot, *other_robot_ptr);
+                     
+                }
+            }
+        }
+
+        void collide(Robot& a, Robot& b)
+        {
+            if(a.hp() == b.hp())
+            {
+                a.kill();
+                b.kill();
+                return;
+            }
+            if(a.hp() < b.hp())
+            {
+                a.kill();
+                b.damage();
+                return;
+            }
+            else
+            {
+                a.damage();
+                b.kill();
+                return;
+            }
+        }
+
+        list<Robot> m_robots;
+    };
+
+public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) 
     {
         return first(positions, healths, directions);
