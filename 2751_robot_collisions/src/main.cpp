c4a976c612b613d29f94107f427cdecba71e7de3
rawrex <rawrex@proton.me>
2024-07-13 13:55:51 +0700

    Added the basic infrastructure for the solution.

--- 2751_robot_collisions/src/main.cpp
+++ 2751_robot_collisions/src/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Solution 
+{
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) 
+    {
+        return first(positions, healths, directions);
+    }
+
+private:
+    vector<int> first(vector<int>& positions, vector<int>& healths, const string& directions)
+    {
+
+        return {};
+    }
+};
 
 int main()
 {
