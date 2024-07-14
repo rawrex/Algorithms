5f4c2cdc08ba2f0fcb731f41d226250007b19fd8
rawrex <rawrex@proton.me>
2024-07-14 22:53:24 +0700

    Some drafts of appraches were added.

--- 2751_robot_collisions/src/main.cpp
+++ 2751_robot_collisions/src/main.cpp
@@ -1,56 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <list>
 
 using namespace std;
 
+struct Robot
+{
+    Robot(int i, int pos, int hp, const string& dir) : m_original_index(i), m_position(pos), m_hp(hp), m_increment(dir == "R" ? 1 : -1) 
+    {
+
+    }
+
+    int onTick()
+    {
+        return m_position += m_increment; 
+    }
+
+    int position() const noexcept
+    {
+        return m_position;
+    }
+
+    int hp() const noexcept
+    {
+        return m_hp;
+    }
+
+    void kill() noexcept
+    {
+        m_hp = 0;
+    }
+
+    void damage() noexcept
+    {
+        --m_hp;
+    }
+
+    bool isDead() const noexcept
+    {
+        return m_hp == 0;
+    }
+
+private:
+    int m_original_index = 0;
+    int m_position = 0;
+    int m_hp = 0;
+    int m_increment = 0;
+};
+
 class Solution 
 {
-    struct Robot
-    {
-        Robot(int pos, int hp, const string& dir) : m_position(pos), m_hp(hp), m_increment(dir == "R" ? 1 : -1) 
-        {
-
-        }
-
-        int onTick()
-        {
-            return m_position += m_increment; 
-        }
-
-        int position() const noexcept
-        {
-            return m_position;
-        }
-
-        int hp() const noexcept
-        {
-            return m_hp;
-        }
-
-        void kill() noexcept
-        {
-            m_hp = 0;
-        }
-
-        void damage() noexcept
-        {
-            --m_hp;
-        }
-
-        bool isDead() const noexcept
-        {
-            return m_hp == 0;
-        }
-
-    private:
-        int m_position = 0;
-        int m_hp = 0;
-        int m_increment = 0;
-    };
 
     struct Scene
     {
-        void tick() 
+        bool tick() 
         {
             unordered_map<int, Robot*> position2robot;
 
@@ -65,7 +69,12 @@ class Solution
                     collide(current_robot, *other_robot_ptr);
                 }
             }
+
+            if(done())
+                return false;
+
             position2robot.clear();
+            return true;
         }
 
         void collide(Robot& a, Robot& b)
@@ -90,6 +99,15 @@ class Solution
             }
         }
 
+        bool done()
+        {
+            // ... ?
+            for(const auto& robot : m_robots)
+            {
+                
+            }
+        }
+
         list<Robot> m_robots;
     };
 
@@ -105,6 +123,76 @@ private:
 
         return {};
     }
+};
+
+struct Solution2
+{
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, const string& directions) 
+    {
+        auto size = static_cast<int>(positions.size());
+
+        list<int> moving_left_indices; 
+        list<int> moving_right_indices; 
+
+        for(int i = 0; i != size; ++i)
+        {
+            if(directions[i] == 'R')
+                moving_right_indices.push_back(i);
+            else
+                moving_left_indices.push_back(i);
+        }
+
+        while(!done())
+        {
+
+        }
+
+        return {};
+    }
+
+    bool done() 
+    {
+       // left is empty 
+       // right is empty 
+       // positions are safe
+    }
+};
+
+
+struct Solution2
+{
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, const string& directions) 
+    {
+        auto total_size = static_cast<int>(positions.size());
+
+        for(int i = 0; i != total_size; ++i)
+        {
+            if(directions[i] == 'L')
+                m_left.emplace(i, positions[i], healths[i], directions[i]);
+            else
+                m_right.emplace(i, positions[i], healths[i], directions[i]);
+        }
+
+        std::sort(m_left.beging(), m_left.end(), [](const auto& a, const auto& b) { return a.m_position < b.m_position; });
+        std::sort(m_right.beging(), m_right.end(), [](const auto& a, const auto& b) { return a.m_position < b.m_position; });
+
+        auto min_size = std::min(m_left.size(), m_right.size());
+        
+        while(!done())
+        {
+            
+        }
+    }
+
+    bool done() 
+    {
+       // left is empty 
+       // right is empty 
+       // positions are safe
+    }
+private:
+    std::vector<Robot> m_left;
+    std::vector<Robot> m_right;
 };
 
 int main()
