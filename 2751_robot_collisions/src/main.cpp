3cb1238138f67e741a939d6544658e2cc63dfae8
rawrex <rawrex@proton.me>
2024-07-17 12:26:20 +0700

    Introduced a draft for a more sane approach with stack usage.

--- 2751_robot_collisions/src/main.cpp
+++ 2751_robot_collisions/src/main.cpp
@@ -1,8 +1,10 @@
 #include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
 #include <iostream>
 #include <vector>
 #include <list>
+#include <stack>
 
 using namespace std;
 
@@ -60,202 +62,222 @@ private:
 	int m_hp = 0;
 	int m_increment = 0;
 };
+    
+struct Scene
+{
+	Scene(const std::vector<int>& p, const std::vector<int>& hp, const std::string& d)
+	{
+		auto size = static_cast<int>(p.size());
+
+		for (int i = 0; i != size; ++i)
+			m_robots.emplace_back(i, p[i], hp[i], d[i]);
+	}
+
+	void tick()
+	{
+		unordered_map<int, Robot*> current_positions;
+		//            unordered_map<int, Robot*> new_positions;
+
+		for (auto& current_robot : m_robots)
+		{
+			if (current_robot.isDead())
+				continue;
+
+			current_positions.emplace(current_robot.position(), &current_robot);
+		}
+
+		for (auto& current_robot : m_robots)
+		{
+			if (current_robot.isDead())
+				continue;
+
+			auto current_robot_new_position = current_robot.onTick();
+
+			if (!current_positions.emplace(current_robot_new_position, &current_robot).second)
+			{
+				auto& other_robot_ptr = current_positions[current_robot_new_position];
+
+				if (other_robot_ptr->isDead())
+					continue;
+
+				if (other_robot_ptr->direction() != current_robot.direction())
+					collide(current_robot, *other_robot_ptr);
+			}
+
+			//                if(!new_positions.emplace(new_position, &current_robot).second)
+			//                {
+			//                    auto& other_robot_ptr = new_positions[new_position];
+			//                    if(other_robot_ptr->direction() != current_robot.direction() && !other_robot_ptr->isDead())
+			//                        collide(current_robot, *other_robot_ptr);
+			//                }
+		}
+	}
+
+	void collide(Robot& a, Robot& b) const noexcept
+	{
+		if (a.hp() == b.hp())
+		{
+			a.kill();
+			b.kill();
+			return;
+		}
+		if (a.hp() < b.hp())
+		{
+			a.kill();
+			b.damage();
+			return;
+		}
+		else
+		{
+			a.damage();
+			b.kill();
+			return;
+		}
+	}
+
+	bool done()
+	{
+		int max_left_position = std::numeric_limits<int>::min();
+		int min_right_position = std::numeric_limits<int>::max();
+
+		for (const auto& robot : m_robots)
+		{
+			if (robot.isDead())
+				continue;
+
+			if (robot.direction() == 'L')
+				max_left_position = std::max(robot.position(), max_left_position);
+			else
+				min_right_position = std::min(robot.position(), min_right_position);
+		}
+
+		// There are no left-moving robots in front of any of the right moving
+		return max_left_position < min_right_position;
+	}
+
+	std::vector<int> finish() const
+	{
+		std::vector<Robot> alive;
+
+		for (auto& robot : m_robots)
+			if (!robot.isDead())
+				alive.emplace_back(robot);
+
+		std::sort(alive.begin(), alive.end(), [](const auto& a, const auto& b) { return a.originalPosition() < b.originalPosition(); });
+
+		std::vector<int> result;
+		for (const auto& r : alive)
+			result.emplace_back(r.hp());
+
+		return result;
+	}
+
+	vector<Robot> m_robots;
+};
+
+vector<int> naive(vector<int>& positions, vector<int>& healths, string directions)
+{
+	Scene scene(positions, healths, directions);
+
+	while (!scene.done())
+		scene.tick();
+
+	return scene.finish();
+}
 
 class Solution
 {
-    struct Scene
+    vector<int> stackSolution(const vector<int>& positions, const vector<int>& healths, const string& directions) const
     {
-        Scene(const std::vector<int>& p, const std::vector<int>& hp, const std::string& d)
+        struct Robot 
         {
-            auto size = static_cast<int>(p.size());
+            int index = 0;
+            int pos = 0;
+            int hp = 0;
+            char dir = 0;
+        };
 
-            for (int i = 0; i != size; ++i)
-                m_robots.emplace_back(i, p[i], hp[i], d[i]);
-        }
+        stack<Robot> R;
+        stack<Robot> L;
+		vector<int> healths_copy = healths;
+        auto size = positions.size();
 
-        void tick()
+        for (int i = 0; i != size; ++i)
         {
-            unordered_map<int, Robot*> current_positions;
-//            unordered_map<int, Robot*> new_positions;
+            if (healths_copy[i] == 0)
+                continue;
 
-            for (auto& current_robot : m_robots)
+			Robot robot = { i, positions[i], healths_copy[i], directions[i] };
+            
+            if (robot.dir == 'R')
             {
-                if(current_robot.isDead())
-                    continue;
-
-                current_positions.emplace(current_robot.position(), &current_robot);
-            }
-
-            for(auto& current_robot : m_robots)
-            {
-                if(current_robot.isDead())
-                    continue;
-
-                auto current_robot_new_position = current_robot.onTick();
-
-                if (!current_positions.emplace(current_robot_new_position, &current_robot).second)
+                if (L.empty())
+                    R.push(robot);
+                else
                 {
-                    auto& other_robot_ptr = current_positions[current_robot_new_position];
-
-                    if (other_robot_ptr->isDead())
-                        continue;
-
-                    if(other_robot_ptr->direction() != current_robot.direction())
-                        collide(current_robot, *other_robot_ptr);
+                    const auto& other = L.top();
+					if (robot.hp < other.hp)
+					{
+						healths_copy[robot.index] = 0;
+						--healths_copy[other.index];
+					}
+                    else if (robot.hp > other.hp)
+                    {
+						--healths_copy[robot.index];
+						healths_copy[other.index] = 0;
+                        R.push(robot);
+                    }
+                    else
+                    {
+						healths_copy[robot.index] = 0;
+						healths_copy[other.index] = 0;
+                        L.pop();
+                    }
                 }
-
-//                if(!new_positions.emplace(new_position, &current_robot).second)
-//                {
-//                    auto& other_robot_ptr = new_positions[new_position];
-//                    if(other_robot_ptr->direction() != current_robot.direction() && !other_robot_ptr->isDead())
-//                        collide(current_robot, *other_robot_ptr);
-//                }
-            }
-        }
-
-        void collide(Robot& a, Robot& b) const noexcept
-        {
-            if(a.hp() == b.hp())
-            {
-                a.kill();
-                b.kill();
-                return;
-            }
-            if(a.hp() < b.hp())
-            {
-                a.kill();
-                b.damage();
-                return;
             }
             else
             {
-                a.damage();
-                b.kill();
-                return;
-            }
-        }
-
-        bool done()
-        {
-            int max_left_position = std::numeric_limits<int>::min();
-            int min_right_position = std::numeric_limits<int>::max();
-
-            for(const auto& robot : m_robots)
-            {
-                if (robot.isDead())
-                    continue;
-
-                if (robot.direction() == 'L')
-                    max_left_position = std::max(robot.position(), max_left_position);
+                if (R.empty())
+                    L.push(robot);
                 else
-                    min_right_position = std::min(robot.position(), min_right_position);
+                {
+                    const auto& other = R.top();
+					if (robot.hp < other.hp)
+					{
+						healths_copy[robot.index] = 0;
+						--healths_copy[other.index];
+					}
+                    else if (robot.hp > other.hp)
+                    {
+						--healths_copy[robot.index];
+						healths_copy[other.index] = 0;
+                        L.push(robot);
+                    }
+                    else
+                    {
+						healths_copy[robot.index] = 0;
+						healths_copy[other.index] = 0;
+                        R.pop();
+                    }
+                }
             }
-
-            // There are no left-moving robots in front of any of the right moving
-            return max_left_position < min_right_position;
         }
 
-        std::vector<int> finish() const
+        vector<int> resulting_hp;
+        for (int i = 0; i != size; ++i)
         {
-            std::vector<Robot> alive;
-
-            for (auto& robot : m_robots)
-                if(!robot.isDead())
-                    alive.emplace_back(robot);
-
-            std::sort(alive.begin(), alive.end(), [](const auto& a, const auto& b) { return a.originalPosition() < b.originalPosition(); });
-
-            std::vector<int> result;
-            for (const auto& r : alive)
-                result.emplace_back(r.hp());
-
-            return result;
+			if(healths_copy[i])
+				resulting_hp.emplace_back(healths_copy[i]);
         }
 
-        vector<Robot> m_robots;
-    };
+        return resulting_hp;
+    }
 
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) 
     {
-        Scene scene(positions, healths, directions);
-
-        while (!scene.done())
-            scene.tick();
-
-        return scene.finish();
+        return stackSolution(positions, healths, directions);
     }
-};
-
-struct Solution2
-{
-    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, const string& directions) 
-    {
-        auto size = static_cast<int>(positions.size());
-
-        list<int> moving_left_indices; 
-        list<int> moving_right_indices; 
-
-        for(int i = 0; i != size; ++i)
-        {
-            if(directions[i] == 'R')
-                moving_right_indices.push_back(i);
-            else
-                moving_left_indices.push_back(i);
-        }
-
-        while(!done())
-        {
-
-        }
-
-        return {};
-    }
-
-    bool done() 
-    {
-       // left is empty 
-       // right is empty 
-       // positions are safe
-        return true;
-    }
-};
-
-struct Solution3
-{
-    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, const string& directions) 
-    {
-        auto total_size = static_cast<int>(positions.size());
-
-        for(int i = 0; i != total_size; ++i)
-        {
-            if(directions[i] == 'L')
-                m_left.emplace_back(i, positions[i], healths[i], directions[i]);
-            else
-                m_right.emplace_back(i, positions[i], healths[i], directions[i]);
-        }
-
-        std::sort(m_left.begin(), m_left.end(), [](const auto& a, const auto& b) { return a.position() < b.position(); });
-        std::sort(m_right.begin(), m_right.end(), [](const auto& a, const auto& b) { return a.position() < b.position(); });
-
-        auto min_size = std::min(m_left.size(), m_right.size());
-        
-        while(!done())
-        {
-            
-        }
-    }
-
-    bool done() 
-    {
-       // left is empty 
-       // right is empty 
-       // positions are safe
-        return true;
-    }
-private:
-    std::vector<Robot> m_left;
-    std::vector<Robot> m_right;
 };
 
 //vector<int> positions = {1, 2, 5, 6};
