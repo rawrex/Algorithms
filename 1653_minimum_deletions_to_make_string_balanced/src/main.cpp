da8dd021a6a6534efa2fffa50e181a8dce7feb17
rawrex <rawrex@proton.me>
2024-07-31 14:13:38 +0700

    Initial commit for the solution.

---
+++ 1653_minimum_deletions_to_make_string_balanced/src/main.cpp
@@ -0,0 +1,12 @@
+class Solution 
+{
+public:
+    int minimumDeletions(const string& s)
+    {
+        auto size = s.size();
+        for(size_t forward_index = 0, backward_index = size; forward_index != size, backward_index != 0; ++forward_index, --backward_index)
+        {
+
+        }
+    }
+};
