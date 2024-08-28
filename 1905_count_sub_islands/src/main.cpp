f3e65c60cee9e358828cf94c1c95b2697d55c726
rawrex <rawrex@proton.me>
2024-08-28 22:46:37 +0700

    Added a faulty solution.

--- 1905_count_sub_islands/src/main.cpp
+++ 1905_count_sub_islands/src/main.cpp
@@ -3,32 +3,67 @@
 
 using namespace std;
 
-template<typename T>
-using matrix_type = vector<vector<T>>;
-
-template<typename T>
-using size_type = matrix_type<T>::size_type;
-
 struct Solution 
 {
-    int countSubIslands(matrix_type<int>& grid1, matrix_type<int>& grid2) 
-    {
-        for (size_type<int> i = 0; i != grid1.size(); ++i)
-        {
-            // subscript warning
-            for (size_type<int> j = 0; j != grid1[0].size(); ++j)
-            {
+	using matrix_type = vector<vector<int>>;
 
+    int countSubIslands(matrix_type& grid1, matrix_type& grid2) 
+    {
+        if (grid1.empty())
+            return 0;
+
+        m_grid1 = &grid1;
+        m_grid2 = &grid2;
+        m_rows_n = grid1.size();
+        m_cols_n = grid1[0].size();
+
+        int counter = 0;
+
+        for (int row = 0; row != m_rows_n; ++row)
+        {
+            for (int col = 0; col != m_cols_n; ++col)
+            {
+				if(isIsland(*m_grid2, row, col) && dfs(row, col))
+					++counter;
             }
         }
-        return 0;
+
+        return counter;
     }
 
 private:
-    bool dfs(matrix_type<int>& grid1, matrix_type<int>& grid2)
+    bool dfs(int row, int col) 
     {
-        
+        bool is_subisland = isIsland(*m_grid1, row, col);
+        if (!is_subisland)
+            return false;
+
+        markProcessed(*m_grid2, row, col);
+
+        if(!(dfs(row - 1, col) && dfs(row + 1, col) && dfs(row, col - 1) && dfs(row, col + 1)))
+            is_subisland = false;
+
+        return is_subisland;
     }
+
+    inline bool isIsland(const matrix_type& grid, int row, int col) const noexcept
+    {
+        if (row < 0 || row >= m_rows_n || col < 0 || col >= m_cols_n)
+            return false;
+
+        return grid[row][col];
+    }
+
+    void markProcessed(matrix_type& grid, int row, int col) noexcept
+    {
+        grid[row][col] = 0;
+    }
+
+    matrix_type* m_grid1;
+    matrix_type* m_grid2;
+
+    int m_rows_n = 0;
+    int m_cols_n = 0;
 };
 
 int main()
