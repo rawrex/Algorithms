19c36ac91e842c900d733f4717a4342fcc9a0d15
rawrex <rawrex@proton.me>
2024-08-28 14:12:20 +0700

    Added a first outline for the solution.

--- 1905_count_sub_islands/src/main.cpp
+++ 1905_count_sub_islands/src/main.cpp
@@ -3,12 +3,31 @@
 
 using namespace std;
 
+template<typename T>
+using matrix_type = vector<vector<T>>;
+
+template<typename T>
+using size_type = matrix_type<T>::size_type;
+
 struct Solution 
 {
-    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) 
+    int countSubIslands(matrix_type<int>& grid1, matrix_type<int>& grid2) 
     {
-    
+        for (size_type<int> i = 0; i != grid1.size(); ++i)
+        {
+            // subscript warning
+            for (size_type<int> j = 0; j != grid1[0].size(); ++j)
+            {
+
+            }
+        }
         return 0;
+    }
+
+private:
+    bool dfs(matrix_type<int>& grid1, matrix_type<int>& grid2)
+    {
+        
     }
 };
 
