85020e9a292748fa820960bf2054a4830e6b5bc0
rawrex <rawrex@proton.me>
2024-09-01 14:26:51 +0700

    Slight simplifaction of the approach, removed one excessive loop.

--- 1380_lucky_number_in_matrix/src/main.cpp
+++ 1380_lucky_number_in_matrix/src/main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 class Solution
 {
+	using index_t = vector<int>::size_type;
+
 public:
 	vector<int> luckyNumbers(vector<vector<int>>& input)
 	{
@@ -15,48 +17,47 @@ public:
 	}
 
 private:
-	vector<int> naive(const vector<vector<int>>& input) const
+	vector<int> naive(const vector<vector<int>>& input)
 	{
-		using index_t = vector<int>::size_type;
+		index_t rows_n = input.size();
+		index_t cols_n = input[0].size();
 
-		vector<int> result;
-		unordered_map<index_t, int> row_tracker;
-		unordered_map<index_t, int> col_tracker;
+		// Prefill the trackers with sentinel values
+		row_tracker.reserve(rows_n);
+		for (index_t row_i = 0; row_i != rows_n; ++row_i)
+			row_tracker[row_i] = std::numeric_limits<int>::max();
 
-		for (index_t row_i = 0; row_i != input.size(); ++row_i)
+		col_tracker.reserve(cols_n);
+		for (index_t col_i = 0; col_i != cols_n; ++col_i)
+			col_tracker[col_i] = std::numeric_limits<int>::min();
+
+		// Collect the extreme values for each axis
+		for (index_t row_i = 0; row_i != rows_n; ++row_i)
 		{
-			int row_min = std::numeric_limits<int>::max();
-
-			for (index_t col_i = 0; col_i != input[0].size(); ++col_i)
-				row_min = std::min(input[row_i][col_i], row_min);
-
-			row_tracker[row_i] = row_min;
+			for (index_t col_i = 0; col_i != cols_n; ++col_i)
+			{
+				row_tracker[row_i] = std::min(row_tracker[row_i], input[row_i][col_i]);
+				col_tracker[col_i] = std::max(col_tracker[col_i], input[row_i][col_i]);
+			}
 		}
 
-		for (index_t col_i = 0; col_i != input[0].size(); ++col_i)
+		// Collect the lucky numbers
+		for (index_t row_i = 0; row_i != rows_n; ++row_i)
 		{
-			int col_max = std::numeric_limits<int>::min();
-
-			for (index_t row_i = 0; row_i != input.size(); ++row_i)
-				col_max = std::max(input[row_i][col_i], col_max);
-
-			col_tracker[col_i] = col_max;
-		}
-
-		for (index_t row_i = 0; row_i != input.size(); ++row_i)
-		{
-			for (index_t col_i = 0; col_i != input[0].size(); ++col_i)
+			for (index_t col_i = 0; col_i != cols_n; ++col_i)
 			{
 				if (row_tracker[row_i] == col_tracker[col_i])
-				{
 					result.emplace_back(input[row_i][col_i]);
-					break;
-				}
 			}
 		}
 
 		return result;
 	}
+
+private:
+	vector<int> result;
+	unordered_map<index_t, int> row_tracker;
+	unordered_map<index_t, int> col_tracker;
 };
 
 int main()
