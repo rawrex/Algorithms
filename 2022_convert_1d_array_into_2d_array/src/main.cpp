3ffade5d232de7ccf7a280b84a85f10c16133aba
rawrex <rawrex@proton.me>
2024-09-01 14:46:19 +0700

    Added a first approach. Fails to preallocate and preinitialize the resulting 2D vector as I wish.

--- 2022_convert_1d_array_into_2d_array/src/main.cpp
+++ 2022_convert_1d_array_into_2d_array/src/main.cpp
@@ -4,8 +4,26 @@ using namespace std;
 
 struct Solution
 {
-	vector<vector<int>> construct2DArray(vector<int>& original, int m, int n)
+	vector<vector<int>> construct2DArray(const vector<int>& original, int rows_n, int cols_n) const
 	{
-		
+		if (auto size = original.size(); size > (static_cast<decltype(original.size())>(rows_n) * cols_n))
+			return {};
+
+		auto result = vector<vector<int>>(rows_n, vector<int>(cols_n, 0));
+
+		for (int i = 0; i != original.size(); ++i)
+		{
+			auto row_i = i / rows_n;
+			auto col_i = i % cols_n;
+			
+			result[row_i][col_i] = original[i];
+		}
+
+		return result;
 	}
 };
+
+int main()
+{
+
+}
