b01de9535cc58b827e1771322cd4bb196369e614
rawrex <rawrex@proton.me>
2024-09-01 15:28:07 +0700

    Added an okay solution.

--- 2022_convert_1d_array_into_2d_array/src/main.cpp
+++ 2022_convert_1d_array_into_2d_array/src/main.cpp
@@ -6,17 +6,15 @@ struct Solution
 {
 	vector<vector<int>> construct2DArray(const vector<int>& original, int rows_n, int cols_n) const
 	{
-		if (auto size = original.size(); size > (static_cast<decltype(original.size())>(rows_n) * cols_n))
+		if(auto size = original.size(); size != (static_cast<decltype(original.size())>(rows_n) * cols_n))
 			return {};
 
-		auto result = vector<vector<int>>(rows_n, vector<int>(cols_n, 0));
+		auto result = vector<vector<int>>(rows_n, vector<int>(cols_n));
 
-		for (int i = 0; i != original.size(); ++i)
+		for (int row_i = 0; row_i != rows_n; ++row_i)
 		{
-			auto row_i = i / rows_n;
-			auto col_i = i % cols_n;
-			
-			result[row_i][col_i] = original[i];
+			for (int col_i = 0; col_i != cols_n; ++col_i)
+				result[row_i][col_i] = original[row_i * cols_n + col_i];
 		}
 
 		return result;
