2264c479519de8855adc9c971eefe7f7e92abdf8
rawrex <rawrex@proton.me>
2024-09-02 21:52:13 +0700

    Implemented a naive solution (gets the time limit).

--- 1894_find_the_student_who_will_replace_the_chalk/src/main.cpp
+++ 1894_find_the_student_who_will_replace_the_chalk/src/main.cpp
@@ -4,13 +4,28 @@ using namespace std;
 
 struct Solution 
 {
-    int chalkReplacer(vector<int>& chalk, int k) 
+    int chalkReplacer(const vector<int>& chalk, int k) 
     {
+        int size = chalk.size();
+        int index = 0;
 
+        while (k > 0)
+        {
+            if (index == size)
+                index = 0;
+
+            if (k < chalk[index])
+                return index;
+
+            k -= chalk[index];
+            ++index;
+        }
+
+        return index == size ? 0 : index;
     }
 };
 
 int main()
 {
-
+    Solution().chalkReplacer({3, 4, 1, 2}, 25);
 }
