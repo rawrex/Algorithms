36f9ccd92669e9c0ccc7be87e15d6003df8edb06
rawrex <rawrex@proton.me>
2024-09-08 17:06:40 +0700

    Added final solution.

--- 725_split_linked_list_in_parts/src/main.cpp
+++ 725_split_linked_list_in_parts/src/main.cpp
@@ -12,42 +12,44 @@ struct ListNode
 	ListNode* next;
 };
 
-struct Solution 
+struct Solution
 {
     vector<ListNode*> splitListToParts(ListNode* head, int k)
     {
         vector<ListNode*> result(k, nullptr);
 
-        // Count the total number of nodes
-        size_t total_count = 0;
-        for (auto node = head; node; node = node->next)
+        int total_count = 0;
+        for (ListNode* node = head; node; node = node->next)
             ++total_count;
 
-        auto part_size = total_count / k;
-        auto remainder = total_count % k;
+        int part_size = total_count / k;
+        int remainder = total_count % k;
 
         auto node = head;
         for (int i = 0; i < k && node; ++i)
         {
             result[i] = node;
 
+            // Distribute the reaminder
             int current_part_size = part_size + (remainder-- > 0 ? 1 : 0);
 
-            for (int j = 1; j != current_part_size && node; ++j)
-                node = node->next;
+            // Move the pointer to the end of the current part
+            for (int j = 1; j < current_part_size; ++j)
+                if (node) node = node->next;
 
-            // Move to the next part
+            // Disconnect the current part from the rest of the list
             if (node)
             {
-                auto next_part_node = node->next;
+                ListNode* next_part_node = node->next;
                 node->next = nullptr;
-                node = next_part_node; 
+                node = next_part_node;
             }
         }
 
         return result;
     }
 };
+
 
 int main()
 {
