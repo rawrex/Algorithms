29e66ee7552e6c673e150bb293e293cbcb78f3bc
rawrex <rawrex@proton.me>
2024-09-12 12:59:54 +0700

    Added a oneliner solution to the leetcode#1684.

--- 1684_count_the_number_of_consistent_strings/src/main.cpp
+++ 1684_count_the_number_of_consistent_strings/src/main.cpp
@@ -1,30 +1,43 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <unordered_set>
 
 using namespace std;
 
 struct Solution 
 {
-    int countConsistentStrings(string allowed_str, const vector<string>& words) 
+    int countConsistentStrings(const string& allowed_str, const vector<string>& words) 
     {
-        unordered_set<char> allowed(std::begin(allowed_str), std::end(allowed_str));
-        size_t counter_inconsistent = 0;
-
-        for (const auto& word : words)
-        {
-            for (char ch : word)
-            {
-                if (!allowed.contains(ch))
-                {
-                    ++counter_inconsistent;
-                    break;
-                }
-            }
-        }
-
-        return words.size() - counter_inconsistent;
+		// return naive(allowed_str, words);
+		return oneline(allowed_str, words);
     }
+
+private:
+	int oneline(const string& allowed_str, const vector<string>& words)
+	{
+		return words.size() - std::count_if(std::begin(words), std::end(words), [&allowed_str](const string_view& word) { return word.find_first_not_of(allowed_str) != std::string::npos; });
+	}
+
+    int naive(string allowed_str, const vector<string>& words) 
+	{
+		unordered_set<char> allowed(std::begin(allowed_str), std::end(allowed_str));
+		size_t counter_inconsistent = 0;
+
+		for (const auto& word : words)
+		{
+			for (char ch : word)
+			{
+				if (!allowed.contains(ch))
+				{
+					++counter_inconsistent;
+					break;
+				}
+			}
+		}
+
+		return words.size() - counter_inconsistent;
+	}
 };
 
 int main()
