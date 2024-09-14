564f4fb84754d48da7828ff207e34464a0133d2f
rawrex <rawrex@proton.me>
2024-09-14 12:52:20 +0700

    Initial commit with an outline of the solution for the leetcode#2419.

---
+++ 2419_longest_subarray_with_maximum_bitwise_and/src/main.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+struct Solution
+{
+    int longestSubarray(vector<int>& nums) 
+    {
+        auto size = nums.size();
+        int beg = 0;
+        int end = 0;
+        int sum = 0;
+    
+        for (int i = 0; i != size; ++i)
+        {
+                        
+        }
+
+        return end - beg;
+    }
+};
+
+int main()
+{
+
+}
