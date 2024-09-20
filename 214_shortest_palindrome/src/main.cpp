6326ae09a60604b381bb1e3b88c1d3d865c0407d
rawrex <rawrex@proton.me>
2024-09-20 14:00:38 +0700

    Added processing for the cases when we have an even palindrome as a result.

--- 214_shortest_palindrome/src/main.cpp
+++ 214_shortest_palindrome/src/main.cpp
@@ -18,21 +18,34 @@ struct Solution
     }
 
 private:
-    int findPalindromeMiddle(const string& s) const
+    int findPalindromeMiddle(const string& s)
     {
         for (int middle = m_size / 2; middle >= 0; --middle)
         {
             int left_index = middle - 1;
             int right_index = middle + 1;
-
-            for (/* empty */; left_index >= 0 && right_index < m_size; ++right_index, --left_index)
+            while (left_index >= 0 && right_index < m_size && s[left_index] == s[right_index])
             {
-                if (s[left_index] != s[right_index])
-                    break;
+                --left_index;
+                ++right_index;
             }
 
             if (left_index < 0)
                 return middle;
+
+            left_index = middle - 1;
+            right_index = middle;
+            while (left_index >= 0 && right_index < m_size && s[left_index] == s[right_index])
+            {
+                --left_index;
+                ++right_index;
+            }
+
+            if (left_index < 0)
+            {
+                m_is_even = true;
+                return middle;
+            }
         }
 
         return 0;
@@ -40,21 +53,22 @@ private:
 
     string makePalindrome(const string& input, int middle) const
     {
-        deque<char> result;// (2 * m_size - middle, ' ');
+        deque<char> result;
 
-        for (int i = m_size - 1; i != middle; --i)
+        for (int i = m_size - 1; i >= middle; --i)
             result.push_front(input[i]);
 
-        for (int i = middle; i != m_size; ++i)
+        for (int i = m_is_even ? middle : middle + 1; i != m_size; ++i)
             result.push_front(input[i]);
 
         return { std::begin(result), std::end(result) };
     }
 
     int m_size = 0;
+    bool m_is_even = false;
 };
 
 int main()
 {
-    Solution().shortestPalindrome("aacecaaa");
+    Solution().shortestPalindrome("abbacd");
 }
