2cad638a1991c75a85ce8d3219ccf297cbf6c82e
rawrex <rawrex@proton.me>
2024-09-20 12:40:58 +0700

    Initial commit on the leetcode#214.

---
+++ 214_shortest_palindrome/src/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+using namespace std;
+
+struct Solution 
+{
+    string shortestPalindrome(string s) 
+    {
+        
+    }
+
+private:
+    string naive(string s)
+    {
+
+    }
+};
+
+int main()
+{
+
+}
