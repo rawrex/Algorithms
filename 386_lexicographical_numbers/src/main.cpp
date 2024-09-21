79f92f2d7e8d6b59a799077a2e5d6f157d7957e1
rawrex <rawrex@proton.me>
2024-09-21 10:25:42 +0700

    Added a less crude solution to the leetcode#386.

--- 386_lexicographical_numbers/src/main.cpp
+++ 386_lexicographical_numbers/src/main.cpp
@@ -8,10 +8,45 @@ struct Solution
 {
     vector<int> lexicalOrder(int n) 
     {
-        return naive(n);
+        // return naive(n);
+        return second(n);
     }
 
 private:
+    vector<int> second(int n)
+    {
+        auto sorter = [](int a, int b) 
+        {
+            int a_position = 1;
+            for (int i = a; i; i /= 10)
+                a_position *= 10;
+
+            int b_position = 1;
+            for (int i = b; i; i /= 10)
+                b_position *= 10;
+
+            int a_digit = 0;
+            int b_digit = 0;
+            for (/* empty */; a_position && b_position; a_position /= 10, b_position /= 10)
+            {
+                a_digit = a / a_position;
+                b_digit = b / b_position;
+
+                if (a_digit == b_digit)
+                    continue;
+            }
+
+            return a_digit < b_digit;
+        };
+
+        multiset<int, decltype(sorter)> sorted(sorter);
+
+        for (int i = 1; i <= n; ++i)
+            sorted.emplace(i);
+
+        return { std::cbegin(sorted), std::cend(sorted) };
+    }
+
     vector<int> naive(int n)
     {
         string a_str;
@@ -36,5 +71,5 @@ private:
 
 int main()
 {
-
+    Solution().lexicalOrder(13);
 }
