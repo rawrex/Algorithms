34fe05bece81cc680adb08362178789e3f60a991
rawrex <rawrex@proton.me>
2024-09-21 09:44:54 +0700

    Added a naive solutioin to the leetcode#386.

--- 386_lexicographical_numbers/src/main.cpp
+++ 386_lexicographical_numbers/src/main.cpp
@@ -1,4 +1,6 @@
+#include <set>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -6,7 +8,29 @@ struct Solution
 {
     vector<int> lexicalOrder(int n) 
     {
+        return naive(n);
+    }
 
+private:
+    vector<int> naive(int n)
+    {
+        string a_str;
+        string b_str;
+
+        auto sorter = [&a_str, &b_str](int a, int b) 
+        { 
+            a_str = std::to_string(a);
+            b_str = std::to_string(b);
+
+            return a_str < b_str;
+        };
+
+        set<int, decltype(sorter)> sorted(sorter);
+
+        for (int i = 1; i <= n; ++i)
+            sorted.emplace(i);
+
+        return { std::cbegin(sorted), std::cend(sorted) };
     }
 };
 
