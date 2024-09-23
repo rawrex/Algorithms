fd5cd74d35a657988706c4c7cc730877c62515cc
rawrex <rawrex@proton.me>
2024-09-23 13:17:01 +0700

    Added a copy paste (optimal) solution.

--- 2707_extra_characters_in_a_string/src/main.cpp
+++ 2707_extra_characters_in_a_string/src/main.cpp
@@ -1,65 +1,92 @@
 #include <vector>
 #include <string>
-#include <unordered_map>
+#include <memory>
+#include <unordered_set>
 
 using namespace std;
 
-struct Trie 
+struct Solution 
 {
-    Trie(const std::vector<std::string>& dictionary)
+    int minExtraChar(const string& s, const vector<string>& dictionary)
     {
-        buildFromVector(dictionary, this);
-    }
-
-    void insert(const std::string& word) 
-    {
-        Trie* node = this;
-        for (char ch : word) 
-        {
-            if (!node->children.contains(ch)) 
-                node->children[ch] = new Trie();
-
-            node = node->children[ch];
-        }
-        node->m_is_end_of_word = true;
-    }
-
-    // Builds a trie from a vector of words
-    static void buildFromVector(const std::vector<std::string>& words, Trie* trie) 
-    {
-        for (const auto& word : words) 
-            trie->insert(word);
-    }
-
-    // Function to check if there is any word in the trie that starts with the given prefix
-    bool startsWith(const std::string& prefix) const 
-    {
-        const Trie* node = this;
-        for (char ch : prefix) 
-        {
-            if (!node->children.contains(ch)) 
-                return false;
-
-            node = node->children.at(ch);
-        }
-        return true;
+        // return naive(s, dictionary);
+        return foo(s, dictionary);
     }
 
 private:
-    std::unordered_map<char, Trie*> children;
-    bool m_is_end_of_word = false;
-};
-
-
-struct Solution 
-{
-    int minExtraChar(string s, vector<string>& dictionary) 
+    int naive(string s, const vector<string>& dictionary)
     {
-                        
+        m_tracker = { cbegin(dictionary), cend(dictionary) };
+
+        int beg = 0;
+        int end = 0;
+        while (check(s, beg, end))
+        {
+            s.erase(beg, end - beg + 1);
+    
+            // Reset indices after removal
+            beg = 0;  
+            end = 0;
+        }
+
+        return s.size();
     }
+
+    bool check(const string& s, int& beg, int& end)
+    {
+        for (int i = 0; i < s.size(); ++i)
+        {
+            for (int j = i; j < s.size(); ++j)
+            {
+                auto substring = s.substr(i, j - i + 1);
+                if (m_tracker.contains(substring))
+                {
+                    beg = i;
+                    end = j;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+	int foo(const string& s, const vector<string>& dictionary) 
+    {
+        m_tracker = { std::cbegin(dictionary), std::cend(dictionary) };
+
+		auto size = s.size();
+		vector<int> dp(size + 1);
+
+        // Base case: no extra character is needed for an empty substring.
+		dp[0] = 0;
+
+		// Calculate the minimum number of extra characters for each substring ending at position 'i'.
+		for (int i = 1; i <= size; ++i) 
+        {
+			// Initialize dp[i] assuming all previous characters are from the dictionary and only s[i-1] is extra.
+			dp[i] = dp[i - 1] + 1;
+
+			// Check all possible previous positions 'j' to see if s[j, i-1] is a word in the dictionary.
+			for (int j = 0; j < i; ++j) 
+            {
+				if (m_tracker.contains(s.substr(j, i - j))) 
+                {
+					// If it is a word, update dp[i] to be the minimum of its current value
+					// and dp[j], which represents the minimum number of extra characters needed to form substring s[0...j-1].
+					dp[i] = min(dp[i], dp[j]);
+				}
+			}
+		}
+
+		// This holds the minimum number of extra characters needed for the entire string.
+		return dp[size];
+	}
+
+	unordered_set<string> m_tracker;
 };
 
 int main()
 {
-
+    Solution().minExtraChar("sayhelloworld", { "hello", "world" });
 }
