e55d15b926794647106ea59b135af9d142010c17
rawrex <rawrex@proton.me>
2024-09-26 18:41:57 +0700

    Added solution to the leetcode#729.

--- 729_my_calendar_I/src/main.cpp
+++ 729_my_calendar_I/src/main.cpp
@@ -1,16 +1,28 @@
+#include <map>
 #include <set>
 
 struct MyCalendar 
 {
-    MyCalendar() 
+    struct Event
     {
+        friend bool operator<(const Event& a, const Event& b) noexcept
+        {
+            return a.m_start < b.m_start && a.m_end <= b.m_start;
+        }
 
-    }
+        int m_start = -1;
+        int m_end = -1;
+    };
+
+    MyCalendar() = default;
 
     bool book(int start, int end) 
     {
-
+        return m_events.emplace(start, end).second;
     }
+
+private:
+    std::set<Event> m_events;
 };
 
 int main()
