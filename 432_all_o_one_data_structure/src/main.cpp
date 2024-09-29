fa5f22c090613bb49a011dbe103ea7893590958e
rawrex <rawrex@proton.me>
2024-09-29 11:59:31 +0700

    Minor clean up.

--- 432_all_o_one_data_structure/src/main.cpp
+++ 432_all_o_one_data_structure/src/main.cpp
@@ -9,9 +9,13 @@ class AllOne
 {
     struct Bucket
     {
+        Bucket(int c) : count(c) 
+        {
+
+        }
+
         int count;
         std::unordered_set<std::string> keys;
-        Bucket(int c) : count(c) {}
     };
 
 public:
@@ -20,35 +24,30 @@ public:
     void inc(const std::string& key)
     {
         // If the key is already in the map, increment its count
-        if (m_data.count(key))
+        if (m_data.contains(key))
         {
-            auto current_bucket_it = m_data[key];
-            auto next_bucket_it = std::next(current_bucket_it);
+            auto current_bucket_iter = m_data[key];
+            auto next_bucket_iter = std::next(current_bucket_iter);
 
             // If the next bucket doesn't exist or its count is not current+1, create it
-            if (next_bucket_it == m_buckets.end() || next_bucket_it->count != current_bucket_it->count + 1)
-            {
-                next_bucket_it = m_buckets.insert(next_bucket_it, Bucket(current_bucket_it->count + 1));
-            }
+            if (next_bucket_iter == m_buckets.end() || next_bucket_iter->count != current_bucket_iter->count + 1)
+                next_bucket_iter = m_buckets.emplace(next_bucket_iter, current_bucket_iter->count + 1);
 
             // Move the key to the next bucket
-            next_bucket_it->keys.insert(key);
-            m_data[key] = next_bucket_it;
+            next_bucket_iter->keys.insert(key);
+            m_data[key] = next_bucket_iter;
 
             // Remove the key from the current bucket
-            current_bucket_it->keys.erase(key);
-            if (current_bucket_it->keys.empty())
-            {
-                m_buckets.erase(current_bucket_it);
-            }
+            current_bucket_iter->keys.erase(key);
+            if (current_bucket_iter->keys.empty())
+                m_buckets.erase(current_bucket_iter);
         }
         else
         {
             // If the key is new, insert it into the bucket with count 1
             if (m_buckets.empty() || m_buckets.begin()->count != 1)
-            {
                 m_buckets.emplace_front(1);
-            }
+
             m_buckets.begin()->keys.insert(key);
             m_data[key] = m_buckets.begin();
         }
@@ -57,61 +56,54 @@ public:
     void dec(const std::string& key)
     {
         // If the key exists in the map, decrement its count
-        if (m_data.count(key))
+        if (m_data.contains(key))
         {
-            auto current_bucket_it = m_data[key];
+            auto current_bucket_iter = m_data[key];
 
             // If the current count is 1, we remove the key entirely
-            if (current_bucket_it->count == 1)
+            if (current_bucket_iter->count == 1)
             {
-                current_bucket_it->keys.erase(key);
+                current_bucket_iter->keys.erase(key);
                 m_data.erase(key);
 
                 // If the bucket is empty, remove the bucket
-                if (current_bucket_it->keys.empty())
-                {
-                    m_buckets.erase(current_bucket_it);
-                }
+                if (current_bucket_iter->keys.empty())
+                    m_buckets.erase(current_bucket_iter);
+
                 return;
             }
 
             // Otherwise, move the key to the previous bucket (count - 1)
-            auto prev_bucket_it = std::prev(current_bucket_it);
+            auto prev_bucket_iter = std::prev(current_bucket_iter);
 
             // If the previous bucket doesn't exist or its count is not current-1, create it
-            if (current_bucket_it == m_buckets.begin() || prev_bucket_it->count != current_bucket_it->count - 1)
-            {
-                prev_bucket_it = m_buckets.insert(current_bucket_it, Bucket(current_bucket_it->count - 1));
-            }
+            if (current_bucket_iter == m_buckets.begin() || prev_bucket_iter->count != current_bucket_iter->count - 1)
+                prev_bucket_iter = m_buckets.emplace(current_bucket_iter, current_bucket_iter->count - 1);
 
             // Move the key to the previous bucket
-            prev_bucket_it->keys.insert(key);
-            m_data[key] = prev_bucket_it;
+            prev_bucket_iter->keys.insert(key);
+            m_data[key] = prev_bucket_iter;
 
             // Remove the key from the current bucket
-            current_bucket_it->keys.erase(key);
-            if (current_bucket_it->keys.empty())
-            {
-                m_buckets.erase(current_bucket_it);
-            }
+            current_bucket_iter->keys.erase(key);
+            if (current_bucket_iter->keys.empty())
+                m_buckets.erase(current_bucket_iter);
         }
     }
 
     std::string getMaxKey()
     {
         if (m_buckets.empty())
-        {
-            return "";
-        }
+            return {};
+
         return *(m_buckets.back().keys.begin());
     }
 
     std::string getMinKey()
     {
         if (m_buckets.empty())
-        {
-            return "";
-        }
+            return {};
+
         return *(m_buckets.front().keys.begin());
     }
 
