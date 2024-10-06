b71e4bc560a9def9a73a29942404f99e759f34d0
rawrex <rawrex@proton.me>
2024-10-06 14:03:14 +0700

    Corrections to the space skipping approach.

--- 1813_sentence_similarity_III/main.cpp
+++ 1813_sentence_similarity_III/main.cpp
@@ -11,20 +11,25 @@ struct Solution
         if (s1 == s2)
             return true;
 
+        string word1;
+        string word2;
+
+        vector<string> words1;
+        vector<string> words2;
+
+        stringstream sswords1(s1);
+        while (getline(sswords1, word1, ' '))
+			words1.emplace_back(word1);
+
+        stringstream sswords2(s2);
+        while (getline(sswords2, word2, ' '))
+			words2.emplace_back(word2);
+
         std::vector<string> common_prefix;
         std::vector<string> common_suffix;
 
-        auto iter1 = s1.cbegin();
-        auto iter2 = s2.cbegin();
-
-        auto riter1 = s1.crbegin();
-        auto riter2 = s2.crbegin();
-
-        string word1;
-        string word2;
         stringstream ss1(s1);
         stringstream ss2(s2);
-
         while (getline(ss1, word1, ' ') && getline(ss2, word2, ' '))
         {
             if(word1 == word2)
@@ -37,7 +42,6 @@ struct Solution
         std::reverse(std::begin(s2_reversed), std::end(s2_reversed));
         stringstream ss1_reversed(s1_reversed);
         stringstream ss2_reversed(s2_reversed);
-
         while (getline(ss1_reversed, word1, ' ') && getline(ss2_reversed, word2, ' '))
         {
             if(word1 == word2)
@@ -46,15 +50,17 @@ struct Solution
 
         // Transform the suffix so the characters are going left to right
         std::reverse(std::begin(common_suffix), std::end(common_suffix));
+        for(auto& word : common_suffix)
+            std::reverse(std::begin(word), std::end(word));
 
         auto total = common_prefix;
         total.insert(std::end(total), std::begin(common_suffix), std::end(common_suffix));
 
-        return common_prefix == s1_set || common_prefix == s2_set || common_suffix == s1_set || common_suffix == s2_set || total == s1_set || total == s2_set;
+        return common_prefix == words1 || common_prefix == words2 || common_suffix == words1 || common_suffix == words2 || total == words1 || total == words2;
     }
 };
 
 int main()
 {
-    Solution().areSentencesSimilar("My name is Haley", "My Haley");
+    Solution().areSentencesSimilar("A A", "A aA");
 }
