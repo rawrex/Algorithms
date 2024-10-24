b54a6576ba5a90367514ae469d17565f8684a116
rawrex <rawrex@proton.me>
2024-10-24 14:16:14 +0700

    Added an approach (fails).

--- 951_flip_equivalent_binary_trees/main.cpp
+++ 951_flip_equivalent_binary_trees/main.cpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <unordered_map>
+
 struct TreeNode 
 {
 	int val;
@@ -13,8 +16,48 @@ struct Solution
 {
     bool flipEquiv(TreeNode* root1, TreeNode* root2) 
     {
+		collect(root1, 0, m_levels1);
+		collect(root2, 0, m_levels2);
 
+		return test();
     }
+
+private:
+	bool test()
+	{
+		for (unsigned i = 0; i != m_max_level; ++i)
+		{
+			auto& a = m_levels1[i];
+			auto& b = m_levels2[i];
+
+			if (a == b)
+				continue;
+
+			std::reverse(std::begin(b), std::end(b));
+
+			if (a != b)
+				return false;
+		}
+
+		return true;
+	}
+
+	void collect(TreeNode* node, unsigned level, std::unordered_map<unsigned, std::vector<TreeNode*>>& levels)
+	{
+		if (!node)
+			return;
+
+		m_max_level = std::max(m_max_level, level);
+
+		collect(node->left, level + 1, levels);
+		collect(node->right, level + 1, levels);
+
+		levels[level].emplace_back(node);
+	}
+
+	unsigned m_max_level = 0;
+	std::unordered_map<unsigned, std::vector<TreeNode*>> m_levels1;
+	std::unordered_map<unsigned, std::vector<TreeNode*>> m_levels2;
 };
 
 int main()
