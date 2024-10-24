5924fdf4ad0f56cde06ca60c58d0941dfa416023
rawrex <rawrex@proton.me>
2024-10-24 13:34:10 +0700

    Added initial commit for the leetcode#951.

---
+++ 951_flip_equivalent_binary_trees/main.cpp
@@ -0,0 +1,23 @@
+struct TreeNode 
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+struct Solution 
+{
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) 
+    {
+
+    }
+};
+
+int main()
+{
+
+}
