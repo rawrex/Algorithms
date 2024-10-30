9ed3d90eb6b99bbb17bf7d202a97659e9c79b426
rawrex <rawrex@proton.me>
2024-10-30 21:37:26 +0700

    Added a solution for the leetcode#1671.

--- 1671_minimum_number_of_removals_to_make_mountain_array/main.cpp
+++ 1671_minimum_number_of_removals_to_make_mountain_array/main.cpp
@@ -4,25 +4,49 @@ using namespace std;
 
 struct Solution
 {
-    int minimumMountainRemovals(vector<int>& nums)
+    int minimumMountainRemovals(std::vector<int>& nums)
     {
-        vector<int> increasing;
-        vector<int> decreasing;
+        auto n = static_cast<int>(nums.size());
+        std::vector<int> increasing_lengths(n, 1);
+        std::vector<int> decreasing_lengths(n, 1);
 
-        for (int number : nums)
+        // Build increasing subsequence lengths
+        std::vector<int> increasing;
+        for (int i = 0; i < n; ++i)
         {
-            if (auto iter = std::lower_bound(std::begin(increasing), std::end(increasing), number); iter == std::cend(increasing))
+            int number = nums[i];
+
+            if (auto iter = std::lower_bound(std::begin(increasing), std::end(increasing), number); iter == std::end(increasing))
                 increasing.push_back(number);
             else
                 *iter = number;
 
-            if (auto iter = std::lower_bound(std::begin(decreasing), std::end(decreasing), number); iter == std::cend(decreasing))
+            increasing_lengths[i] = static_cast<int>(increasing.size());
+        }
+
+        // Build decreasing subsequence lengths from right to left
+        std::vector<int> decreasing;
+        for (int i = n - 1; i >= 0; --i)
+        {
+            int number = nums[i];
+
+            if (auto iter = std::lower_bound(std::begin(decreasing), std::end(decreasing), number); iter == std::end(decreasing))
                 decreasing.push_back(number);
             else
                 *iter = number;
+
+            decreasing_lengths[i] = static_cast<int>(decreasing.size());
         }
 
-        return nums.size() - increasing.size() - decreasing.size() + 1;
+        int max_mountain = 0;
+        for (int i = 1; i < n - 1; ++i)
+        {
+			// Valid peak
+            if (increasing_lengths[i] > 1 && decreasing_lengths[i] > 1)
+                max_mountain = std::max(max_mountain, increasing_lengths[i] + decreasing_lengths[i] - 1);
+        }
+
+        return n - max_mountain;
     }
 };
 
