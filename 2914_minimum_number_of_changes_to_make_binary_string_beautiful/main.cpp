cfc6e13eb3949fc71234f1bab454319e7e635a1d
rawrex <rawrex@proton.me>
2024-11-05 13:01:35 +0700

    Added a working but non-optimal solution.

--- 2914_minimum_number_of_changes_to_make_binary_string_beautiful/main.cpp
+++ 2914_minimum_number_of_changes_to_make_binary_string_beautiful/main.cpp
@@ -5,51 +5,24 @@ using namespace std;
 
 struct Solution 
 {
-    int minChanges(string s) 
-    {
-        return process(split(s));
-    }
-
-private:
-    using partitions = std::vector<std::pair<std::string_view, std::string_view>>;
-
-    int process(const partitions& parts)
+    int minChanges(std::string_view s) 
     {
         int sum = 0;
-        for (const auto& [L, R] : parts)
-        {
-            if (L.empty() || R.empty())
-                continue;
+        for (const auto& L_R : split(s))
+			sum += (L_R[0] == L_R[1]) ? 0 : 1;
 
-			auto L_size = L.size();
-			auto R_size = R.size();
-
-			if (L_size == 1 && R_size == 1)
-				sum += (L.back() == R.back()) ? 0 : 1;
-			else
-				sum += process(split(L)) + process(split(R));
-		}
         return sum;
     }
 
+private:
+    using partitions = std::vector<std::string_view>;
+
     partitions split(std::string_view str)
     {
-        partitions result;
-
-        // Recursively split the string into partitions of even length
-        if (str.size() % 2 == 0) 
-        {
-            auto middle = str.size() / 2;
-            result.push_back({ str.substr(0, middle), str.substr(middle) });
-        }
-        else 
-        {
-            // Handle odd length by excluding the last character in the left partition
-            auto middle = (str.size() - 1) / 2;
-            result.push_back({ str.substr(0, middle), str.substr(middle) });
-        }
-
-        return result;
+        partitions parts;
+        for (size_t i = 0; i < str.size(); i += 2)
+            parts.emplace_back(str.substr(i, 2));
+        return parts;
     }
 };
 
