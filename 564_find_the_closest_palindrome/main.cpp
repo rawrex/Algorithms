a4c5a18462d0a3cd4329207bf49e88d6c54f7fa1
rawrex <rawrex@proton.me>
2024-12-13 15:28:14 +0700

    Added a clean optimal solution for the leetcode#564.

--- 564_find_the_closest_palindrome/main.cpp
+++ 564_find_the_closest_palindrome/main.cpp
@@ -1,5 +1,6 @@
-#include <string>
 #include <cmath>
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
@@ -7,50 +8,54 @@ struct Solution
 {
     string nearestPalindromic(const string& number)
     {
-        return naive(number);
-    }
+        input_n = stol(number);
+        long closest = std::numeric_limits<long>::max();
 
-private:
-    string naive(const string& number)
-    {
-        auto n = std::stoul(number);
-        auto up = n + 1;
-        auto down = n - 1;
-
-        for (; !isPalindrome(up); ++up);
-        for (; !isPalindrome(down); --down);
-
-        auto up_distance = up - n;
-        auto down_distance = n - down;
-
-        if (up_distance < down_distance)
-            return std::to_string(up);
-        else
-            return std::to_string(down);
-    }
-
-    bool isPalindrome(unsigned long long x)
-    {
-        if (x < 0)
-            return false;
-
-        if (x == 0)
-            return true;
-
-        int digits_n = static_cast<unsigned long long>(std::log10(x));
-        for (int front = digits_n, back = 0; back <= front; --front, ++back)
+        for (long candidate : getCandidates(number)) 
         {
-            int front_digit = (x / static_cast<unsigned long long>(std::pow(10, front))) % 10;
-            int back_digit = (x / static_cast<unsigned long long>(std::pow(10, back))) % 10;
-
-            if (front_digit != back_digit)
-                return false;
+            if (closest == -1)
+                closest = candidate;
+			else if (abs(candidate - input_n) < abs(closest - input_n) || (abs(candidate - input_n) == abs(closest - input_n) && candidate < closest))
+                closest = candidate;
         }
-        return true;
+
+        return to_string(closest);
     }
+
+    unordered_set<long> getCandidates(const string& n) 
+    {
+        unordered_set<long> candidates;
+        auto length = n.size();
+
+        // the largest number with one less digit and the smallest number with one more digit
+        candidates.emplace((long)pow(10, length - 1) - 1);
+        candidates.emplace((long)pow(10, length) + 1);
+        // Leads to different results
+		// candidates.insert(static_cast<long>(pow(10, length - 1) - 1));
+		// candidates.insert(static_cast<long>(pow(10, length) + 1));
+
+        // Get the first half of the original number and consider the number one less, one greater, and itself
+        long first_half = stol(n.substr(0, (length + 1) / 2));
+
+        for (long i = first_half - 1; i <= first_half + 1; ++i) 
+        {
+			// First half as a string
+            string prefix = to_string(i); 
+
+            // Create a palindrome by appending the reverse of the prefix, considering odd/even length
+            string candidate = prefix + string(prefix.rbegin() + (length & 1), prefix.rend());
+
+            candidates.emplace(stol(candidate));
+        }
+
+        candidates.erase(input_n);
+        return candidates;
+    }
+
+    long input_n = 0;
 };
 
 int main()
 {
-
+    Solution().nearestPalindromic("10000000000000000");
 }
